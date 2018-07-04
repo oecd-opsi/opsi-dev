b1e5f672ec8d07b0ad3ea82b0af12a08d314b947
Ben Guhin <benguhin@gmail.com>
2018-07-04 17:36:00 -0500

    conditional header in page.php

--- page.php
+++ page.php
@@ -1,13 +1,21 @@
-<?php get_header();
 
-    global $post;
-    
-    $has_sidebar = 0;
-    $layout = get_post_meta($post->ID, 'layout', true);
-    if($layout != 'fullpage' && is_active_sidebar( 'sidebar' )) {
-      $has_sidebar = 3;
-    }
-    
+
+
+  <?php
+
+      if(is_page_template( 'single-toolkit.php' )) {
+        get_header('toolkits');
+      }
+      else {
+        get_header();
+        $has_sidebar = 0;
+        $layout = get_post_meta($post->ID, 'layout', true);
+        if($layout != 'fullpage' && is_active_sidebar( 'sidebar' )) {
+          $has_sidebar = 3;
+        }
+      }
+      global $post;
+
   ?>
 
 
@@ -18,22 +26,22 @@
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                   <!-- Display featured image in right-aligned floating div -->
               <div class="single_img_wrap <?php echo (!has_post_thumbnail() ? 'noimg' : ''); ?>">
-                <?php 
+                <?php
                   if ( has_post_thumbnail()) {
                     $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large');
                     $img_info =  wp_get_attachment_metadata( get_post_thumbnail_id(get_the_ID()) );
-                   
+
                     echo '
                     <a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" class="featuredimglink fancybox" >';
-                    echo get_the_post_thumbnail(get_the_ID(), 'blog'); 
+                    echo get_the_post_thumbnail(get_the_ID(), 'blog');
                     echo '</a>';
-                   
+
                     if ($img_info['image_meta']['caption'] != '') {
                       echo '<p>'. $img_info['image_meta']['caption'] .'</p>';
                     }
                   }
                 ?>
-                <?php 
+                <?php
                   if (!is_front_page()) {
                     echo (get_field('hide_social_sharing') === true ? '' : wpfai_social());
                   }
@@ -46,12 +54,12 @@
                 <h2 class="entry-subtitle"><?php echo get_field('subheader'); ?></h2>
               <?php } ?>
               <div class="entry-content"><?php the_content(); ?></div>
-              
+
               <?php get_template_part( 'blocks' ); ?>
           </article>
       <?php // comments_template(); ?>
       <?php endwhile; ?>
-      
+
     </div>
 
   <?php wp_reset_query(); ?>
