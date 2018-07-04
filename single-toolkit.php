d5798fe25c71057d3036b38037dcff2ab107738b
Ben Guhin <benguhin@gmail.com>
2018-07-04 17:04:59 -0500

    test template markup

--- single-toolkit.php
+++ single-toolkit.php
@@ -9,6 +9,32 @@
   ?>
 
 
+  <section id="top-section">
+    <div id="image-section" class="col-md-5 col-sm-5 col-xs-12">
+      <div class="tookit-image">
+        <!-- this is where the photo goes -->
+        <p>Placeholder content instead of image</p>
+      </div>
+    </div>
+    <div id="intro-section" class="col-md-6 col-sm-6 col-xs-12">
+      <h1 class="toolkit-title">Title</h1>
+      <h6>URL</h6>
+      <p>Description</p>
+      <div class="row">
+        <div class="meta-column col-md-3 col-sm-3 col-xs-6">
+          <p>Author</p>
+        </div>
+        <div class="meta-column col-md-3 col-sm-3 col-xs-6">
+          <p>Type</p>
+        </div>
+      </div>
+    </div>
+    <div id="save-and-share" class="col-md-1 col-sm-1 col-xs-12">
+      <p>Up-vote</p>
+      <p>Share</p>
+      <p>Save for later</p>
+    </div>
+  </section>
 
 
   <?php while ( have_posts() ) : the_post(); $postid = get_the_ID();
