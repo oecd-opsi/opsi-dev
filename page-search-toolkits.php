2f6e9c280c68887690832e6fffc45df78a1ecf1c
Ben Guhin <benguhin@gmail.com>
2018-07-06 10:03:39 -0500

    add fields

--- page-search-toolkits.php
+++ page-search-toolkits.php
@@ -34,6 +34,45 @@
         <!-- the loop -->
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
         <h2><?php the_title(); ?></h2>
+
+<!-- current image handling -->
+<div id="image-section" class="col-md-4 col-sm-4 col-xs-12">
+  <div class="toolkit-image <?php echo (!has_post_thumbnail() ? 'noimg' : ''); ?>">
+    <?php
+     if ( has_post_thumbnail()) {
+      $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large');
+      $img_info =  wp_get_attachment_metadata( get_post_thumbnail_id(get_the_ID()) );
+
+      echo '
+      <a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" class="featuredimglink fancybox" >';
+      echo get_the_post_thumbnail(get_the_ID(), 'blog');
+      echo '</a>';
+
+      if ($img_info['image_meta']['caption'] != '') {
+        echo '<p>'. $img_info['image_meta']['caption'] .'</p>';
+      }
+     }
+    ?>
+  </div>
+</div>
+<!-- end current image handling -->
+
+<p class="toolkit-description">
+  <?php the_field('description'); ?>
+</p>
+
+<h5>Discipline or practice</h5>
+<?php
+$disciplines = get_field('discipline-or-practice');
+if( $disciplines ): ?>
+  <?php foreach( $disciplines as $discipline ): ?>
+    <p>
+    <a href="<?php echo get_term_link( $discipline ); ?>"><?php echo $discipline->name; ?></a>
+    </p>
+  <?php endforeach; ?>
+<?php endif; ?>
+
+
         <?php endwhile; ?>
         <!-- end of the loop -->
 
