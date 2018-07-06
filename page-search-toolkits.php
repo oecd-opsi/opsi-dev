6b295a0af2a59ea8709688c2341fb6b0d38999e4
Ben Guhin <benguhin@gmail.com>
2018-07-06 09:29:40 -0500

    new query format

--- page-search-toolkits.php
+++ page-search-toolkits.php
@@ -16,7 +16,7 @@
 
               <h1 class="entry-title"><?php the_title(); ?></h1>
 
-              <div class="entry-content"><?php the_content(); ?></div>
+              <div class="entry-content"><?php // the_content(); ?></div>
 
 
           </article>
@@ -24,11 +24,28 @@
       <?php endwhile; ?>
 
 
-                    <?php
-                    $args = array('post_type' => 'toolkit');
-                    $args['search_filter_id'] = 1414;
-                    $query = new WP_Query($args);
-                    ?>
+      <?php
+
+// the query
+$the_query = new WP_Query( $args ); ?>
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+<!-- pagination here -->
+
+<!-- the loop -->
+<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+<h2><?php the_title(); ?></h2>
+<?php endwhile; ?>
+<!-- end of the loop -->
+
+<!-- pagination here -->
+
+<?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
     </div>
 
