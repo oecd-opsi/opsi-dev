d54589c1d64b092d240e68388020d81625ab5324
Ben Guhin <benguhin@gmail.com>
2018-07-06 11:55:38 -0500

    remove content, add results id

--- page-search-toolkits.php
+++ page-search-toolkits.php
@@ -16,10 +16,10 @@
 
               <h1 class="entry-title"><?php the_title(); ?></h1>
 
-              <div class="entry-content"><?php  the_content(); ?></div>
+              <div class="entry-content"><?php // the_content(); ?></div>
 
 
-              <div id="filters-section" class="col-md-4 col-sm-4 col-xs-12">
+              <div id="filters-section" class="filters-section col-md-4 col-sm-4 col-xs-12">
 
               <?php
 
@@ -47,7 +47,7 @@ echo do_shortcode('[searchandfilter id="1414"]');
 
         <?php if ( $the_query->have_posts() ) : ?>
 
-          <div id="results-section" class="col-md-9 col-sm-9 col-xs-12">
+          <div id="search-filter-results-1414" class="results-section col-md-9 col-sm-9 col-xs-12">
 
 
         <!-- pagination here -->
