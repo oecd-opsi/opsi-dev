b609a3c9338a1aa75de7f0f31f48b6717967cfe3
Ben Guhin <benguhin@gmail.com>
2018-07-06 09:33:59 -0500

    keep content for filters

--- page-search-toolkits.php
+++ page-search-toolkits.php
@@ -16,7 +16,30 @@
 
               <h1 class="entry-title"><?php the_title(); ?></h1>
 
-              <div class="entry-content"><?php // the_content(); ?></div>
+              <div class="entry-content"><?php the_content(); ?></div>
+
+              <?php
+
+        // the query
+        $the_query = new WP_Query( $args ); ?>
+
+        <?php if ( $the_query->have_posts() ) : ?>
+
+        <!-- pagination here -->
+
+        <!-- the loop -->
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php endwhile; ?>
+        <!-- end of the loop -->
+
+        <!-- pagination here -->
+
+        <?php wp_reset_postdata(); ?>
+
+        <?php else : ?>
+        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
 
 
           </article>
@@ -24,28 +47,7 @@
       <?php endwhile; ?>
 
 
-      <?php
 
-// the query
-$the_query = new WP_Query( $args ); ?>
-
-<?php if ( $the_query->have_posts() ) : ?>
-
-<!-- pagination here -->
-
-<!-- the loop -->
-<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php endwhile; ?>
-<!-- end of the loop -->
-
-<!-- pagination here -->
-
-<?php wp_reset_postdata(); ?>
-
-<?php else : ?>
-<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
 
     </div>
 
