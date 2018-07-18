8a4e2de9d0ad6bb588258ad93ef0aefb7931b45e
Ben Guhin <benguhin@gmail.com>
2018-07-17 20:04:45 -0500

    guide section template

--- single-guide.php
+++ single-guide.php
@@ -1,96 +1,58 @@
-<?php get_header();
-
+<?php get_header('toolkits');
+    // note change
     global $post;
 
-    $has_sidebar = 0;
-    $layout = get_post_meta($post->ID, 'layout', true);
-    if($layout != 'fullpage' && is_active_sidebar( 'sidebar' )) {
-      $has_sidebar = 3;
-    }
 
   ?>
 
 
 
-    <div class="col-sm-<?php echo 12 - $has_sidebar; ?> <?php echo ($has_sidebar > 0 ? 'col-sm-pull-3' : ''); ?>">
 
-  <?php while ( have_posts() ) : the_post(); $postid = get_the_ID();
 
-    $userid = get_the_author_meta('ID');
-    $job = xprofile_get_field_data( 'Job Title', $userid);
-    $name = xprofile_get_field_data( 'Name', $userid);
 
-  ?>
+  <?php while ( have_posts() ) : the_post(); $postid = get_the_ID(); ?>
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                   <!-- Display featured image in right-aligned floating div -->
-              <div class="single_img_wrap <?php echo (!has_post_thumbnail() ? 'noimg' : ''); ?>">
-                <?php
-                 if ( has_post_thumbnail()) {
-                  $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'large');
-                  $img_info =  wp_get_attachment_metadata( get_post_thumbnail_id(get_the_ID()) );
 
-                  echo '
-                  <a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" class="featuredimglink fancybox" >';
-                  echo get_the_post_thumbnail(get_the_ID(), 'blog');
-                  echo '</a>';
 
-                  if ($img_info['image_meta']['caption'] != '') {
-                    echo '<p>'. $img_info['image_meta']['caption'] .'</p>';
-                  }
-                 }
-                ?>
-                <?php echo (get_field('hide_social_sharing') === true ? '' : wpfai_social()); ?>
-              </div>
-              <?php if (get_field('hide_page_title') !== true) { ?>
-              <h1 class="entry-title"><?php the_title(); ?></h1>
-              <?php } ?>
-              <?php if (get_field('subheader') !='' ) { ?>
-                <h2 class="entry-subtitle"><?php echo get_field('subheader'); ?></h2>
-              <?php } ?>
+            <section id="guide-header">
 
-              <div class="row">
-                <div class="col-sm-2 col-xs-12 col-sm-push-10">
-                  <div class="text-right-not-xs">
-                    <a href="<?php echo bp_core_get_user_domain( $userid ); ?>" class="single_author_avatar" title="<?php echo $name .' '. __('profile', 'opsi'); ?>">
-                      <?php bp_displayed_user_avatar( array('item_id' => $userid, 'type'=>'full') ); ?>
-                    </a>
-                  </div>
-                </div>
-                <div class="col-md-10 col-sm-10 col-xs-12 col-sm-pull-2">
-                  <div class="authormeta">
-                    <?php echo __('Written by', 'opsi'); ?> <?php the_author_posts_link(); ?> <?php echo ($job !='' ? ', ' : ''); ?><?php echo $job; ?>
-                    <?php // TODO: add author description ?>
-                  </div>
-                </div>
-              </div>
+              <h1 class="guide-title"><?php the_title(); ?></h1>
 
-              <div class="single_post_meta">
-                <div class="row">
-                  <div class="col-sm-6">
-                    <p class="text-left">
-                      <i class="fa fa-calendar" aria-hidden="true"></i> <?php echo get_the_date(); ?>
-                    </p>
-                  </div>
-                  <div class="col-sm-6">
-                    <p class="text-right-not-xs single_category_list">
-                      <i class="fa fa-folder-open-o" aria-hidden="true"></i> <?php echo get_the_category_list(', '); ?>
-                    </p>
-                  </div>
-                </div>
-              </div>
+            </section>
 
-              <div class="entry-content"><?php the_content(); ?></div>
+            <section id="guide-main-content" class="guide-page-content wpb_column vc_column_container col-md-12 vc_col-sm-12">
 
-              <?php get_template_part( 'blocks' ); ?>
+              <h2 id="intro-text" class="category-header text-header">Welcome to the OPSI Meta-Toolkit</h2>
+              <p class="browse-page-content">
+                Toolkits are a great way to share innovative methods and practices. A plethora of free innovation toolkits, playbooks and guides exist to help people identify, develop and practice necessary skills and apply new ways of reaching an outcome. We built this “Meta-Toolkit” to help you find the ones best suited to you and your situation. <a href="#how-it-works">Learn more</a>
+              </p>
 
-              <div class="post_tags">
-                <?php echo get_the_tag_list(); ?>
-              </div>
+            </section>
+
+
+
+            <hr id="browse-section-rule">
+
+            <section id="another-section" class="browse-page-content category-row wpb_column vc_column_container col-md-12 vc_col-sm-12">
+
+              <?php the_content(); ?>
+
+            </section>
+
+
+
+
+
+
+
           </article>
-      <?php comments_template(); ?>
+      <?php // comments_template(); ?>
       <?php endwhile; ?>
 
-    </div>
+
+
+
 
   <?php wp_reset_query(); ?>
 
