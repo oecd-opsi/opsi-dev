63c9ad405caea97944a21017eaefb7fb6bb76c97
Ben Guhin <benguhin@gmail.com>
2018-07-22 00:50:03 -0500

    add referral section to guides

--- single-guide.php
+++ single-guide.php
@@ -29,7 +29,22 @@
 
             <section id="guide-sidebar" class="guide-page-content wpb_column vc_column_container col-md-4 vc_col-sm-12">
 
-              <p>sidebar goes here</p>
+              <section id="referral-section" class="toolkit-section col-md-12 col-sm-12 col-xs-12">
+                <div class="row">
+                  <div class="meta-column col-md-6 col-sm-6 col-xs-12">
+                    <div id="cases-referral-block" class="referral-block">
+                      <h5>See cases from others doing this in government</h5>
+                      <p><a href="/our-work/case-studies/">Go to case studies</a></p>
+                    </div>
+                  </div>
+                  <div class="meta-column col-md-6 col-sm-6 col-xs-12">
+                    <div id="cases-referral-block" class="referral-block">
+                      <h5>Find experts and advisers who can assist me with this</h5>
+                      <p><a href="/about-observatory-of-public-sector-innovation/about-our-in-country-contacts/">Go to advice</a></p>
+                    </div>
+                  </div>
+                </div>
+              </section>
 
             </section>
 
