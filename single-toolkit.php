ab5d4869729251c121db571a1dba0cc2081a0634
Ben Guhin <benguhin@gmail.com>
2018-07-21 20:05:13 -0500

    test

--- single-toolkit.php
+++ single-toolkit.php
@@ -239,7 +239,7 @@
            $args = array(
              'post_type'   => 'toolkit',
              'post_status' => 'publish',
-             'post__not_in' => array($currentID), // removes the current page from being shown
+//             'post__not_in' => array($currentID), // removes the current page from being shown
              'posts_per_page' => 4,
              'nopaging' => true,
              'tax_query'   => array(
